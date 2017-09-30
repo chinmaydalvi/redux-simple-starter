2757b556947f691d0f668fb3d993fca89dbc4205
Chinmay Dalvi <chinmaydalvi94@gmail.com>
2017-09-30 20:31:14 +0530

    successfully connect data of redux with react

---
+++ src/containers/book-list.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class BookList extends Component{
+
+	renderList(){
+			return this.props.books.map((book) =>{
+					return (
+						<li key={book.title} className="list-group-item">{book.title}</li>
+					);
+			});
+	}
+
+	render(){
+		 return (
+		 		<ul className="list-group col-sm-4">
+		 				{ this.renderList() }
+		 		</ul>
+		 	)
+	}
+
+}
+
+function mapStateToProps(state) {
+	// This functions take applicate state as parameter here 
+	//application state means book-list and Active Book.
+
+	// And whatever is returned by this function will taken as props
+		return{
+				books: state.books
+		};
+}
+
+export default connect(mapStateToProps)(BookList);
