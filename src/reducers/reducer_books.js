1a9ed3901a5c8769b0589725ed39e206fd4c63e0
Chinmay Dalvi <chinmaydalvi94@gmail.com>
2017-10-10 01:30:02 +0530

    display active book successfully

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 export default function(){
 	return [
-		{ title: 'JavaScript: The Good Parts' },
-		{ title: 'Harry Potter' },
-		{ title: 'The Dark tower' },
-		{ title: 'Eloquoent Ruby' },
-		{ title: 'Eloquoent JavaScript' }
+		{ title: 'JavaScript: The Good Parts', pages: 102 },
+		{ title: 'Harry Potter', pages: 44 },
+		{ title: 'The Dark tower', pages: 22 },
+		{ title: 'Eloquoent Ruby', pages: 999 },
+		{ title: 'Eloquoent JavaScript', pages: 6 }
 
 	];
 }
