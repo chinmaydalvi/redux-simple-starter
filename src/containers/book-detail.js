d31beb77522910414204e058968f16b25e5a3bd7
Chinmay Dalvi <chinmaydalvi94@gmail.com>
2017-10-10 00:39:44 +0530

    added container for book detail

---
+++ src/containers/book-detail.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+import { component } from 'react-redux';
+
+export default class BookDetail extends Component{
+
+	render(){
+			return(
+				<div>Book Detail !!!</div>
+			);
+	}
+}
